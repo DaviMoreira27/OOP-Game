a2d5c4a38a11bdfd3ac3f3577f7e9410367d76f4
DaviMoreira27 <davimsantana2706@gmail.com>
2025-05-11 14:30:20 -0300

    feat: fixed compilation problems, ready to start development

---
+++ src/Modelo/BichinhoVaiVemHorizontal.java
@@ -0,0 +1,28 @@
+package Modelo;
+
+public class BichinhoVaiVemHorizontal extends Personagem {
+
+    private boolean bRight;
+    int iContador;
+
+    public BichinhoVaiVemHorizontal(String sNomeImagePNG) {
+        super(sNomeImagePNG);
+        bRight = true;
+        iContador = 0;
+    }
+
+    public void autoDesenho() {
+        if (iContador == 5) {
+            iContador = 0;
+            if (bRight) {
+                this.setPosicao(pPosicao.getLinha(), pPosicao.getColuna() + 1);
+            } else {
+                this.setPosicao(pPosicao.getLinha(), pPosicao.getColuna() - 1);
+            }
+
+            bRight = !bRight;
+        }
+        super.autoDesenho();
+        iContador++;
+    }
+}
